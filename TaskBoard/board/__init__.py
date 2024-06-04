1e2b906c43b27a7246cc5d7f18578bca80dff856
adrovice <146707587+adrovice@users.noreply.github.com>
2024-06-04 09:26:47 +0200

    Update __init__.py

--- TaskBoard/board/__init__.py
+++ TaskBoard/board/__init__.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://boarduser:morgen000@localhost/boarddb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #app.config['SECRET_KEY'] = 'f1c50cdf58a5ac7024799454'
-app.secret_key = "passwordmy"
+app.secret_key = "pass"
 
 db = SQLAlchemy(app)
 from board import routes
